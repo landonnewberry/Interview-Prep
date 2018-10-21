7891ddcdd0137fe5a1dccf107b841de51db45031
Landon Newberry <landonnewberry@gmail.com>
2018-10-20 22:04:53 -0500

    unrefined hashtable

---
+++ data_structures/hashtable.py
@@ -0,0 +1,43 @@
+from array import array
+
+class HashTable(object):
+    
+    @staticmethod
+    def _default_hash(n):
+        h = 3
+        for c in str(n):
+            h = h * 31 + ord(c)
+        return h
+
+    def __init__(self, hash_function=None, size=10000):
+        self._hash_function = self._default_hash if hash_function == None else hash_function
+        self.size = size
+        self._array = array('I', (0,) * self.size)
+
+    def add(self, n):
+        o = i = self._hash_function(n) % self.size
+        while self._array[i] != 0:
+            if i == (self.size - 1):
+                i = 0
+            elif i == (o - 1):
+                if self._array[o - 1] != 0:
+                    raise Exception("HashTable full")
+                else:
+                    self._array[o - 1] = n
+                    break
+            else:
+                i += 1
+        self._array[i] = n
+
+    def exists(self, n):
+        i = o = self._hash_function(n) % self.size
+        while self._array[i] != n:
+            if i == (self.size - 1):
+                i = 0
+            elif i == (o - 1):
+                return self._array[o - 1] == n
+            elif self._array[i] == 0:
+                return False
+            else:
+                i += 1
+        return True 
